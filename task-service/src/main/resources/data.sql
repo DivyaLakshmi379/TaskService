bd1c49ed14da9e460794611d305595b3c033a9b4
DELL <DELL@host.docker.internal>
2025-10-06 11:52:41 +0530

    data

---
+++ task-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TASK (title, description, status) VALUES 
+('Learn Spring Boot', 'Finish project in 1 day', 'IN_PROGRESS'),
+('Deploy to Render', 'Deploy Spring Boot app using Docker', 'COMPLETED'),
+('Test API', 'Verify /tasks endpoint and H2 database', 'PENDING');
